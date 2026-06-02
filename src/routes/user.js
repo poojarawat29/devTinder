4a19bf0b7e3514f839da7a298a74633f9469b369
poojaa09 <poojarawat0329@gmail.com>
2026-06-02 06:48:18 +0530

    api implementation completed

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const express=require("express")
 const userRouter=express.Router()
 const userAuth=require("../middleware/auth")
 const ConnectionRequest=require("../models/connectionRequest")
-
+const User=require("../models/user")
 
 // get all the pending connection requests from loggedIn user
 userRouter.get("/user/requests/received", userAuth, async(req,res)=>{
@@ -48,4 +48,40 @@ res.json({
         res.status(400).send("ERROR: "+err.message)
     }
 })
-module.exports=userRouter
+
+// feed -> getting all the users except loggedIn user and the users who are already connected with loggedIn user
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page || 1);
+    let limit = parseInt(req.query.limit || 10);
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select("firstName lastName photoURL about age gender skills")
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("ERROR: " + error.message);
+  }
+});
+module.exports = userRouter;
